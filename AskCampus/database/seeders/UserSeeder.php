a4b99b989d264ad050e571ac2cc7d00b7778d6a9
Ayawo II <kossiabel99@gmail.com>
2026-04-20 21:50:43 +0000

    fini

--- AskCampus/database/seeders/UserSeeder.php
+++ AskCampus/database/seeders/UserSeeder.php
@@ -13,29 +13,10 @@ class UserSeeder extends Seeder
         // Compte modérateur
         User::create([
             'name'       => 'Admin Mod',
-            'email'      => 'mod@askcampus.fr',
-            'password'   => Hash::make('password'),
+            'email'      => 'kossiayawoabel@gmail.com',
+            'password'   => Hash::make('kossiayawo'),
             'role'       => 'moderator',
-            'reputation' => 999,
+            'reputation' => 0,
         ]);
-
-        // Étudiants
-        $students = [
-            ['name' => 'Alice Martin',   'email' => 'alice@etud.fr',   'reputation' => 120],
-            ['name' => 'Bob Dupont',     'email' => 'bob@etud.fr',     'reputation' => 45],
-            ['name' => 'Clara Fontaine', 'email' => 'clara@etud.fr',   'reputation' => 230],
-            ['name' => 'David Leroy',    'email' => 'david@etud.fr',   'reputation' => 15],
-            ['name' => 'Emma Petit',     'email' => 'emma@etud.fr',    'reputation' => 80],
-        ];
-
-        foreach ($students as $student) {
-            User::create([
-                'name'       => $student['name'],
-                'email'      => $student['email'],
-                'password'   => Hash::make('password'),
-                'role'       => 'student',
-                'reputation' => $student['reputation'],
-            ]);
-        }
     }
 }
